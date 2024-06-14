835936cf0cda6f14983eb1772b576058836b1573
kyo0802 <e24091077@gs.ncku.edu.tw>
2024-06-14 23:46:10 +0800

    pet_db_v1

--- db/mysql.js
+++ db/mysql.js
@@ -24,35 +24,40 @@ connection.query('CREATE TABLE IF NOT EXISTS account_data (email VARCHAR(30), f_
 connection.query('CREATE TABLE IF NOT EXISTS pet_data (pet VARCHAR(15), gender VARCHAR(10), size VARCHAR(10), age VARCHAR(10), image VARCHAR(255), PRIMARY KEY(pet)) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci');
 connection.query('CREATE TABLE IF NOT EXISTS matches (match_id INT AUTO_INCREMENT PRIMARY KEY,user1_email VARCHAR(30),user2_email VARCHAR(30),match_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,FOREIGN KEY (user1_email) REFERENCES account(email),FOREIGN KEY (user2_email) REFERENCES account(email)) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;');
 connection.query('CREATE TABLE IF NOT EXISTS messages (message_id INT AUTO_INCREMENT PRIMARY KEY,match_id INT,sender_email VARCHAR(30),message_text TEXT,sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,FOREIGN KEY (match_id) REFERENCES matches(match_id),FOREIGN KEY (sender_email) REFERENCES account(email)) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;')
+connection.query('CREATE TABLE IF NOT EXISTS walk_data (walk_id INT AUTO_INCREMENT,pet VARCHAR(15), walk_date DATE,walk_time TIME,duration FLOAT,distance FLOAT, route TINYINT(1),A TINYINT(1),B TINYINT(1),satisfaction TINYINT(1),PRIMARY KEY(walk_id),FOREIGN KEY (pet) REFERENCES pet_data(pet)) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;')
+connection.query('CREATE TABLE IF NOT EXISTS walk_id_data (walk_id INT,rating TINYINT(1),water INT,note VARCHAR(255),master_rating TINYINT(1),happiness TINYINT(1),FOREIGN KEY (walk_id) REFERENCES walk_data(walk_id)) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;')
 
 
-const imagePath = path.join(__dirname, '../images/dogs/156.png');
+const imagePath = path.join(__dirname, '../images/dogs/153.png');
 const query = 'INSERT INTO pet_data (pet, gender, size, age, image) VALUES (?, ?, ?, ?, ?)';
-    const petData = ['rain', 'female', 'medium', 'adult', imagePath];
+const petData = ['Maxi', 'male', 'large', 'adult', imagePath];
 
-    connection.query(query, petData, (error, results) => {
-      if (error) {
-        console.log('Error inserting data:', error);
-      } else {
-        console.log('Data inserted successfully');
-      }
-    });
-function getAllCards() {
-  return new Promise((resolve, reject) => {
-      connection.query('SELECT * FROM pet_data', (error, results) => {
-          if (error) {
-              return reject(error);
-          }
-          resolve(results);
-      });
-  });
-}
+connection.query(query, petData, (error, results) => {
+  if (error) {
+    console.log('Error inserting data:', error);
+  } else {
+     console.log('Data inserted successfully');
+   }
+});
+
+const query_2 = 'INSERT INTO walk_data (pet, walk_date, walk_time, duration, distance, route, A, B, satisfaction) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+const walkdata = ['Maxi', '2024-06-15', '14:30:00', 1.5, 3.2, 1, 0, 1, 1];
+
+connection.query(query_2, walkdata, (error, results) => {
+  if (error) {
+    console.log('Error inserting data:', error);
+  } else {
+     console.log('Data inserted successfully');
+   }
+});
 /*
 connection.query('DROP TABLE IF EXISTS account');
 connection.query('DROP TABLE IF EXISTS account_data');
 connection.query('DROP TABLE IF EXISTS pet_data');
 
+connection.query('DROP TABLE IF EXISTS walk_data');
+
 */
 // Export the connection object
 
-module.exports = { getAllCards, connection };
+module.exports = { connection };
